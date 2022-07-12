0200bb8e8b02da34ee929749231d380226301ac7
AhnByeongHo <qudgh7145@naver.com>
2022-07-12 23:23:20 +0900

    ADD: write 1000 BOJ question
    this question level is Bronze V

---
+++ BOJ/solved-ac/Bronze V/1000.py
@@ -0,0 +1,21 @@
+"""
+A+B
+
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A+B를 출력한다.
+"""
+
+A = 0
+B = 1
+
+input = input()
+result = int(input.split(" ")[0]) + int(input.split(" ")[1])
+print(result)
+
+# 제출 7/12 -> 정답
